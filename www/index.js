c12cd38a1aee7540aaeb4cec0e404cdf2bc8ff67
Jörn Zaefferer <joern.zaefferer@gmail.com>
2020-07-11 21:40:22 +0300

    www/index: add two example scripts, for now with their own buttons
    Rename myFunction() to runCommand() and select the textarea element directly.

--- www/index.js
+++ www/index.js
@@ -1,25 +1,40 @@
 import * as wasm from "wasm-nu";
 
+const data = [
+  { name: "Bob", age: 25 },
+  { name: "Fred", age: 35 },
+];
+const example1 = `echo '${JSON.stringify(data)}' | from json`;
+const example2 = `echo '${JSON.stringify(data)}' | from json | where age > 30`;
+
 async function run_nu(input) {
   return await wasm.run_nu(input);
 }
 
-async function myFunction() {
-  var inputs = document.getElementById("myform").elements;
-  let input = inputs["nuinput"].value;
+var nuinput = document.getElementById("nuinput");
+
+async function runCommand() {
+  var input = nuinput.value;
   let output = await run_nu(input + "| to html");
 
   document.getElementById("demo").innerHTML = output;
 }
 
-const runButton = document.getElementById("run-nu");
-runButton.addEventListener("click", (event) => {
-  myFunction();
+document.getElementById("run-nu").addEventListener("click", (event) => {
+  runCommand();
 });
 
-const text = document.getElementById("myform");
-text.addEventListener("keyup", (event) => {
+document.getElementById("load-example-1").addEventListener("click", (event) => {
+  nuinput.value = example1;
+  runCommand();
+});
+document.getElementById("load-example-2").addEventListener("click", (event) => {
+  nuinput.value = example2;
+  runCommand();
+});
+
+document.getElementById("myform").addEventListener("keyup", (event) => {
   if (event.key == "Enter") {
-    myFunction();
+    runCommand();
   }
 });
