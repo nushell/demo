0dd6af1847453422bc2ab17f18d519b688d9b345
Jonathan turner <jonathan.d.turner@gmail.com>
2020-08-05 16:46:14 +1200

    Add more info to ls

--- src/ls.rs
+++ src/ls.rs
@@ -19,14 +19,14 @@ pub struct Ls;
 #[derive(Deserialize)]
 pub struct LsArgs {
     pub path: Option<Tagged<String>>,
-    pub all: bool,
-    pub full: bool,
-    #[serde(rename = "short-names")]
-    pub short_names: bool,
-    #[serde(rename = "with-symlink-targets")]
-    pub with_symlink_targets: bool,
-    #[serde(rename = "du")]
-    pub du: bool,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Debug)]
+struct DirEntry {
+    pub name: String,
+    pub size: u64,
+    pub isDir: bool,
 }
 
 #[async_trait]
@@ -36,33 +36,11 @@ impl WholeStreamCommand for Ls {
     }
 
     fn signature(&self) -> Signature {
-        Signature::build("ls")
-            .optional(
-                "path",
-                SyntaxShape::Pattern,
-                "a path to get the directory contents from",
-            )
-            .switch("all", "also show hidden files", Some('a'))
-            .switch(
-                "full",
-                "list all available columns for each entry",
-                Some('f'),
-            )
-            .switch(
-                "short-names",
-                "only print the file names and not the path",
-                Some('s'),
-            )
-            .switch(
-                "with-symlink-targets",
-                "display the paths to the target files that symlinks point to",
-                Some('w'),
-            )
-            .switch(
-                "du",
-                "display the apparent directory size in place of the directory metadata size",
-                Some('d'),
-            )
+        Signature::build("ls").optional(
+            "path",
+            SyntaxShape::Pattern,
+            "a path to get the directory contents from",
+        )
     }
 
     fn usage(&self) -> &str {
@@ -79,14 +57,20 @@ impl WholeStreamCommand for Ls {
 
         let s = readdir(args.path.map(|x| x.item).unwrap_or_else(|| "/".to_string()));
 
-        let results: Result<Vec<String>, _> = serde_json::from_str(&s);
+        let results: Result<Result<Vec<DirEntry>, String>, _> = serde_json::from_str(&s);
 
         if let Ok(results) = results {
             let results: Vec<Result<ReturnSuccess, ShellError>> = results
+                .map_err(|e| ShellError::labeled_error(e.clone(), e, name.span))?
                 .into_iter()
                 .map(move |x| {
                     let mut dict = TaggedDictBuilder::new(name.clone());
-                    dict.insert_untagged("name", UntaggedValue::string(x));
+                    dict.insert_untagged("name", UntaggedValue::string(x.name));
+                    dict.insert_untagged(
+                        "type",
+                        UntaggedValue::string(if x.isDir { "Dir" } else { "File" }),
+                    );
+                    dict.insert_untagged("size", UntaggedValue::filesize(x.size));
 
                     ReturnSuccess::value(dict.into_value())
                 })
