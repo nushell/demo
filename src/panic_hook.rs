dd66540afd6d7824c6debf80d25452b1e4904e5e
Jörn Zaefferer <joern.zaefferer@gmail.com>
2020-09-24 11:48:04 +0200

    implement a custom hook to handle panics
    This hook is very similar to the console_error_panic_hook, but it instead binds to a JS module that the "app" exposes and which can show the panic message to the user, along with a shortcut to recover the broken process.
    
    This is still crude, but a huge improvement over the complete lack of feedback (when not looking at devtools).

---
+++ src/panic_hook.rs
@@ -0,0 +1,72 @@
+// adapted from the console_error_panic_hook, to allow showing the panic
+// message to the user, by adding it to the DOM somewhere
+use std::panic;
+
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    // something on the JS side has to expose this global debug object with an error method
+    #[wasm_bindgen(js_namespace = debug)]
+    // an explicit module would be cleaner, but for some reason that isn't working here
+    // #[wasm_bindgen(module = "/www/module.js")]
+    fn error(msg: String);
+
+    type Error;
+
+    #[wasm_bindgen(constructor)]
+    fn new() -> Error;
+
+    #[wasm_bindgen(structural, method, getter)]
+    fn stack(error: &Error) -> String;
+}
+
+fn hook_impl(info: &panic::PanicInfo) {
+    let mut msg = info.to_string();
+
+    // Add the error stack to our message.
+    //
+    // This ensures that even if the `console` implementation doesn't
+    // include stacks for `console.error`, the stack is still available
+    // for the user. Additionally, Firefox's console tries to clean up
+    // stack traces, and ruins Rust symbols in the process
+    // (https://bugzilla.mozilla.org/show_bug.cgi?id=1519569) but since
+    // it only touches the logged message's associated stack, and not
+    // the message's contents, by including the stack in the message
+    // contents we make sure it is available to the user.
+    msg.push_str("\n\nStack:\n\n");
+    let e = Error::new();
+    let stack = e.stack();
+    msg.push_str(&stack);
+
+    // Safari's devtools, on the other hand, _do_ mess with logged
+    // messages' contents, so we attempt to break their heuristics for
+    // doing that by appending some whitespace.
+    // https://github.com/rustwasm/console_error_panic_hook/issues/7
+    msg.push_str("\n\n");
+
+    // Finally, expose the panic with `debug.error`!
+    error(msg);
+}
+
+/// A panic hook for use with
+/// [`std::panic::set_hook`](https://doc.rust-lang.org/nightly/std/panic/fn.set_hook.html)
+/// that logs panics into
+/// [`console.error`](https://developer.mozilla.org/en-US/docs/Web/API/Console/error).
+///
+/// On non-wasm targets, prints the panic to `stderr`.
+pub fn hook(info: &panic::PanicInfo) {
+    hook_impl(info);
+}
+
+/// Set the panic hook the first time this is called. Subsequent
+/// invocations do nothing.
+#[inline]
+pub fn set_once() {
+    use std::sync::Once;
+    static SET_HOOK: Once = Once::new();
+    SET_HOOK.call_once(|| {
+        panic::set_hook(Box::new(hook));
+    });
+}
